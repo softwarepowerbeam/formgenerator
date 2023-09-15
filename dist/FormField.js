c10f279f9d1147fb8c26407baa29715a165f0a26
oscargilfc <oscargil.fernandez@tortoise.dev>
2023-09-15 16:29:00 -0600

    FormField classes

---
+++ dist/FormField.js
@@ -0,0 +1,340 @@
+
+export class FormField {
+    /**
+     * 
+     * @param {Formgenerator} formgenerator 
+     * @param {Object} fieldParams 
+     */
+    constructor(formgenerator, fieldParams) {
+        this.form = formgenerator;
+        this.params = fieldParams;
+        this.prefix = this.form.prefix;
+        this.eventTarget = new EventTarget();
+    }
+    generate() {
+        this.div = $('<div>', { id: `${this.prefix}-div-${this.params.name}` }).addClass(`powerbeamform-div ${this.prefix}-div`);
+    }
+
+    appendTo(parent) {
+        this.div.appendTo(parent);
+        return this;
+    }
+
+    assignStandardAttributes(element) {
+        element.attr({
+            name: this.params.name,
+            required: this.params.required,
+            placeholder: this.params.placeholder,
+            title: this.params.title,
+            step: this.params.step,
+            min: this.params.min,
+            max: this.params.max,
+            size: this.params.min,
+            maxlength: this.params.maxlength,
+            minlength: this.params.minlength,
+            pattern: (this.params.pattern) ? (new RegExp(this.params.pattern, 'g')) : undefined,
+        })
+
+        if (this.params.units) {
+            const subdiv = $('<div>').addClass('powerbeamform-input-units-group');
+            element.after(subdiv);
+            subdiv.append(element);
+            element.attr("data-units", this.params.units);
+            element.addClass('powerbeamform-units');
+            const span = $('<span>').addClass('powerbeamform-units-span').html(this.params.units);
+            subdiv.append(span);
+        }
+
+        if (this.params.required) {
+            element.addClass('powerbeamform-required');
+        }
+
+        if (this.params.validator) {
+            element.addClass('powerbeamform-validate');
+            element.on('change', this.validate.bind(this));
+        }
+
+        if (this.params.confirm) {
+            element.addClass('powerbeamform-validate');
+            element.on('change focusout', this.confirm.bind(this));
+        }
+
+        if (this.params.attributes) {
+            element.attr(this.params.attributes);
+        }
+    }
+
+    async validate() {
+        const value = this.input.val();
+
+        this.label.addClass('powerbeamform-label-validating');
+        this.input.addClass('powerbeamform-validating');
+
+        const response = await this.form.ajaxRequest('post', this.form.targetPath + this.params.validator, { value });
+
+        this.label.removeClass('powerbeamform-label-validating');
+        this.input.removeClass('powerbeamform-validating');
+
+        if (response.valid) {
+            this.label.addClass('powerbeamform-label-valid');
+            this.input.addClass('powerbeamform-valid');
+            this.input[0].setCustomValidity("");
+        } else {
+            this.label.removeClass('powerbeamform-label-valid');
+            this.input.removeClass('powerbeamform-valid');
+            this.input[0].setCustomValidity("Already in use");
+        }
+        this.input[0].reportValidity();
+    }
+
+
+    confirm() {
+        const value = this.input.val();
+
+        const data = this.form.getData();
+
+        if (value === data[this.params.confirm]) {
+            this.label.addClass('powerbeamform-label-valid');
+            this.input.addClass('powerbeamform-valid');
+            this.input[0].setCustomValidity("");
+        } else {
+            this.label.removeClass('powerbeamform-label-valid');
+            this.input.removeClass('powerbeamform-valid');
+            this.input[0].setCustomValidity("Mismatch");
+        }
+        setTimeout(() => {
+            this.input[0].reportValidity();
+        }, 100);
+    }
+
+    on(eventName, callback) {
+        this.eventTarget.addEventListener(eventName, callback)
+    }
+
+    _onchange() {
+        const changeEvent = new Event('change');
+        changeEvent.data = { field: this.params.name, value: this.getValue() };
+        this.eventTarget.dispatchEvent(changeEvent);
+        this.form.eventTarget.dispatchEvent(changeEvent);
+
+        const fieldChangeEvent = new Event(`${this.params.name}_change`);
+        fieldChangeEvent.data = { field: this.params.name, value: this.getValue() };
+        this.form.eventTarget.dispatchEvent(fieldChangeEvent);
+    }
+
+}
+
+export class InputField extends FormField {
+    constructor(formgenerator, fieldParams) {
+        super(formgenerator, fieldParams);
+    }
+    generate() {
+        super.generate();
+        this.label = $('<label>', { for: `${this.prefix}-input-${this.params.name}`, html: this.params.label })
+            .addClass(`powerbeamform-label  ${this.prefix}-label form-label`).appendTo(this.div);
+        if (this.params.required) {
+            this.label.addClass('powerbeamform-label-required');
+        }
+        this.input = $('<input>', {
+            id: `${this.prefix}-input-${this.params.name}`,
+            type: this.params.type
+        }).addClass(`powerbeamform-input ${this.prefix}-input form-control`).appendTo(this.div);
+        this.assignStandardAttributes(this.input);
+        this.input.on('change', _onchange.bind(this));
+        return this;
+    }
+
+    setValue(value) {
+        this.input.val(value);
+    }
+
+    getValue() {
+        return this.input.val();
+    }
+
+
+}
+
+export class CheckboxField extends FormField {
+    constructor(formgenerator, fieldParams) {
+        super(formgenerator, fieldParams);
+    }
+
+    generate() {
+        super.generate();
+        this.div.addClass('form-check');
+        this.input = $('<input>', {
+            id: `${this.prefix}-input-${this.params.name}`,
+            type: 'checkbox',
+            value: '1'
+        }).addClass(`powerbeamform-input  ${this.prefix}-input form-check-input`).appendTo(this.div);
+
+        this.assignStandardAttributes(this.input);
+        if (this.params.attributes) {
+            this.input.attr(this.params.attributes);
+        }
+
+        this.label = $('<label>', { for: `${this.prefix}-input-${this.params.name}`, html: this.params.label })
+            .addClass(`powerbeamform-label  ${this.prefix}-label form-check-label`).appendTo(this.div);
+        if (this.params.required) {
+            this.label.addClass('powerbeamform-label-required');
+        }
+        return this;
+    }
+
+    setValue(value) {
+        if (value) {
+            this.input.prop('checked', true);
+        } else {
+            this.input.prop('checked', false);
+        }
+    }
+
+    getValue() {
+        return (this.input.is(':checked')) ? '1' : undefined;
+    }
+}
+
+export class RadioField extends FormField {
+    constructor(formgenerator, fieldParams) {
+        super(formgenerator, fieldParams);
+    }
+
+    generate() {
+        super.generate();
+        this.fieldset = $('<fieldset>', { html: `<legend>${this.params.label}</legend>` }).appendTo(this.div);
+        this.options = [];
+        this.input = [];
+        for (const optionParams of this.params.options) {
+
+            const option = { value: optionParams.value };
+
+            option.div = $('<div>', { id: `${this.prefix}-div-${this.params.name}-${optionParams.value}` }).addClass(`form-check`).appendTo(this.fieldset);
+            option.input = $('<input>', {
+                id: `${this.prefix}-input-${this.params.name}-${optionParams.value}`,
+                name: this.params.name,
+                value: optionParams.value,
+                type: 'radio',
+                title: optionParams.title,
+                required: this.params.required
+            }).addClass(`powerbeamform-input  ${this.prefix}-input form-check-input`).appendTo(option.div);
+
+            option.label = $('<label>', { for: `${this.prefix}-input-${this.params.name}-${optionParams.value}`, html: optionParams.label })
+                .addClass(`powerbeamform-label  ${this.prefix}-label form-check-label`).appendTo(option.div);
+
+            this.options.push(option);
+            this.input.push(option.input);
+        }
+        return this;
+    }
+    setValue(value) {
+        for (const input of this.input) {
+            if (input.attr('value') === value) {
+                input.prop('checked', true);
+            }
+        }
+    }
+
+    getValue() {
+        for (const input of this.input) {
+            if (input.is(':checked')) {
+                return input.attr('value');
+            }
+        }
+    }
+}
+
+export class SelectField extends FormField {
+    constructor(formgenerator, fieldParams) {
+        super(formgenerator, fieldParams);
+    }
+
+    generate() {
+        super.generate();
+        this.input = $('<select>', { required: this.params.required }).addClass('form-select').appendTo(this.div);
+        this.options = [];
+        for (const optionParams of this.params.options) {
+            const option = $('<option>', {
+                id: `${this.prefix}-option-${this.params.name}-${optionParams.value}`,
+                name: this.params.name,
+                value: optionParams.value,
+                html: optionParams.label,
+            }).addClass(`powerbeamform-input  ${this.prefix}-input form-check-input`).appendTo(this.input);
+            this.options.push(option);
+        }
+        if (this.params.attributes) {
+            this.input.attr(this.params.attributes);
+        }
+        return this;
+    }
+
+    setValue(value) {
+        this.input.val(value);
+    }
+
+    getValue() {
+        return this.input.val();
+    }
+
+}
+
+export class TextareaField extends FormField {
+    constructor(formgenerator, fieldParams) {
+        super(formgenerator, fieldParams);
+    }
+
+    generate() {
+        super.generate();
+        this.label = $('<label>', { for: `${this.prefix}-input-${this.params.name}`, html: this.params.label })
+            .addClass(`powerbeamform-label  ${this.prefix}-label form-label`).appendTo(this.div);
+        if (this.params.required) {
+            this.label.addClass('powerbeamform-label-required');
+        }
+        if (this.params.validate) {
+            this.label.addClass('powerbeamform-label-validate');
+        }
+        this.input = $('<textarea>', {
+            id: `${this.prefix}-input-${this.params.name}`,
+        }).addClass(`powerbeamform-input ${this.prefix}-input form-control`).appendTo(this.div);
+        this.assignStandardAttributes(this.input);
+        if (this.params.attributes) {
+            this.input.attr(this.params.attributes);
+        }
+        return this;
+    }
+
+    setValue(value) {
+        this.input.val(value);
+    }
+
+    getValue() {
+        return this.input.val();
+    }
+}
+
+export class HiddenField extends FormField {
+    constructor(formgenerator, fieldParams) {
+        super(formgenerator, fieldParams);
+    }
+
+    appendTo(parent) {
+        this.input.appendTo(parent);
+        return this;
+    }
+
+    generate() {
+        this.input = $('<input>', {
+            id: `${this.prefix}-input-${this.params.name}`,
+            type: 'hidden'
+        }).addClass(`powerbeamform-input ${this.prefix}-input form-control`).appendTo(this.div);
+        this.assignStandardAttributes(this.input);
+        return this;
+    }
+    setValue(value) {
+        this.input.val(value);
+    }
+
+    getValue() {
+        return this.input.val();
+    }
+}
--- dist/Formgenerator.js
+++ dist/Formgenerator.js
@@ -1,3 +1,4 @@
+import * as FormField from './FormField.js';
 
 export default class Formgenerator {
     /**
@@ -12,13 +13,15 @@ export default class Formgenerator {
         this.fields = {};
         this.eventTarget = new EventTarget();
         this.validators = [];
+        this.fieldClasses = {};
+
         for (const fieldsetParams of this.config.fieldsets) {
             const fieldSet = {
                 label: fieldsetParams.label,
                 fields: []
             };
             for (const fieldParams of fieldsetParams.fields) {
-                this.fields[fieldParams.name] = new PowerbeamField(this, fieldParams);
+                this.fields[fieldParams.name] = new this.fieldClasses[fieldParams.type](this, fieldParams);
                 fieldSet.fields.push(this.fields[fieldParams.name]);
             }
             this.fieldsets.push(fieldSet);
@@ -49,6 +52,11 @@ export default class Formgenerator {
     on(eventName, callback) {
         this.eventTarget.addEventListener(eventName, callback)
     }
+
+    onFieldChange(field, callback) {
+        this.eventTarget.addEventListener(`${field}_change`, callback);
+    }
+
 
     getData() {
         const formdata = new FormData(this.form[0]);
@@ -114,6 +122,12 @@ export default class Formgenerator {
 
         return false;
     }
+
+    _onchange(e) {
+        const beforeSubmitEvent = new Event('beforeSubmit');
+        beforeSubmitEvent.data = data;
+    }
+
     /**
      * 
      * @param {string} method 
@@ -141,277 +155,40 @@ export default class Formgenerator {
         return response.data;
     }
 
+    assignFieldType(inputType, FieldClass) {
+        this.fieldClasses[inputType] = FieldClass;
+    }
+
+    assignDefaultClasses() {
+        const inputTypes = [
+            "text",
+            "password",
+            "email",
+            "number",
+            "url",
+            "tel",
+            "date",
+            "datetime-local",
+            "month",
+            "week",
+            "time",
+            "color",
+            "file",
+            "range",
+            "search"
+        ];
+
+        for (const inputType of inputTypes) {
+            this.assignFieldType(inputType, FormField.InputField);
+        }
+
+        this.assignFieldType('checkbox', FormField.CheckboxField);
+        this.assignFieldType('radio', FormField.RadioField);
+        this.assignFieldType('select', FormField.SelectField);
+        this.assignFieldType('textarea', FormField.TextareaField);
+    }
 }
 
-const inputTypes = [
-    "text",
-    "password",
-    "email",
-    "number",
-    "url",
-    "tel",
-    "date",
-    "datetime-local",
-    "month",
-    "week",
-    "time",
-    "color",
-    "file",
-    "range",
-    "search"
-];
 
 
-class PowerbeamField {
-    /**
-     * 
-     * @param {Formgenerator} formgenerator 
-     * @param {Object} fieldParams 
-     */
-    constructor(formgenerator, fieldParams) {
-        this.form = formgenerator;
-        this.params = fieldParams;
-        this.prefix = this.form.prefix;
-    }
-    generate() {
 
-        this.div = $('<div>', { id: `${this.prefix}-div-${this.params.name}` }).addClass(`powerbeamform-div ${this.prefix}-div`);
-
-        if (inputTypes.includes(this.params.type)) return this.inputGenerator();
-
-        if (this.params.type === 'checkbox') return this.checkBoxGenerator();
-
-        if (this.params.type === 'radio') return this.radioGenerator();
-
-        if (this.params.type === 'select') return this.selectGenerator();
-
-        if (this.params.type === 'textarea') return this.textAreaGenerator();
-
-        return this.customGenerator();
-    }
-
-    appendTo(parent) {
-        this.div.appendTo(parent);
-        return this;
-    }
-
-    assignStandardAttributes(element) {
-        element.attr({
-            name: this.params.name,
-            required: this.params.required,
-            placeholder: this.params.placeholder,
-            title: this.params.title,
-            step: this.params.step,
-            min: this.params.min,
-            max: this.params.max,
-            size: this.params.min,
-            maxlength: this.params.maxlength,
-            minlength: this.params.minlength,
-            pattern: (this.params.pattern) ? (new RegExp(this.params.pattern, 'g')) : undefined,
-        })
-
-        if (this.params.units) {
-            const subdiv = $('<div>').addClass('powerbeamform-input-units-group');
-            element.after(subdiv);
-            subdiv.append(element);
-            element.attr("data-units", this.params.units);
-            element.addClass('powerbeamform-units');
-            const span = $('<span>').addClass('powerbeamform-units-span').html(this.params.units);
-            subdiv.append(span);
-        }
-
-        if (this.params.required) {
-            element.addClass('powerbeamform-required');
-        }
-
-        if (this.params.validator) {
-            element.addClass('powerbeamform-validate');
-            element.on('change', this.validate.bind(this));
-        }
-
-        if (this.params.confirm) {
-            element.addClass('powerbeamform-validate');
-            element.on('change focusout', this.confirm.bind(this));
-        }
-
-        if (this.params.attributes) {
-            element.attr(this.params.attributes);
-        }
-    }
-
-    async validate() {
-        const value = this.input.val();
-
-        this.label.addClass('powerbeamform-label-validating');
-        this.input.addClass('powerbeamform-validating');
-
-        const response = await this.form.ajaxRequest('post', this.form.targetPath + this.params.validator, { value });
-
-        this.label.removeClass('powerbeamform-label-validating');
-        this.input.removeClass('powerbeamform-validating');
-
-        if (response.valid) {
-            this.label.addClass('powerbeamform-label-valid');
-            this.input.addClass('powerbeamform-valid');
-            this.input[0].setCustomValidity("");
-        } else {
-            this.label.removeClass('powerbeamform-label-valid');
-            this.input.removeClass('powerbeamform-valid');
-            this.input[0].setCustomValidity("Already in use");
-        }
-        this.input[0].reportValidity();
-    }
-
-
-    confirm() {
-        const value = this.input.val();
-
-        const data = this.form.getData();
-
-        if (value === data[this.params.confirm]) {
-            this.label.addClass('powerbeamform-label-valid');
-            this.input.addClass('powerbeamform-valid');
-            this.input[0].setCustomValidity("");
-        } else {
-            this.label.removeClass('powerbeamform-label-valid');
-            this.input.removeClass('powerbeamform-valid');
-            this.input[0].setCustomValidity("Mismatch");
-        }
-        setTimeout(() => {
-            this.input[0].reportValidity();
-        }, 100);
-    }
-
-    setValue(value) {
-        switch (this.params.type) {
-            case 'checkbox':
-                if (value === "1") {
-                    this.input.prop('checked', true);
-                } else {
-                    this.input.prop('checked', false);
-                }
-                break;
-            case 'radio':
-                for (const input of this.input) {
-                    if (input.attr('value') === value) {
-                        input.prop('checked', true);
-                    }
-                }
-
-                break;
-            case 'file':
-
-                break;
-            default:
-                this.input.val(value);
-                break;
-        }
-
-    }
-
-    inputGenerator() {
-        this.label = $('<label>', { for: `${this.prefix}-input-${this.params.name}`, html: this.params.label })
-            .addClass(`powerbeamform-label  ${this.prefix}-label form-label`).appendTo(this.div);
-        if (this.params.required) {
-            this.label.addClass('powerbeamform-label-required');
-        }
-        this.input = $('<input>', {
-            id: `${this.prefix}-input-${this.params.name}`,
-            type: this.params.type
-        }).addClass(`powerbeamform-input ${this.prefix}-input form-control`).appendTo(this.div);
-        this.assignStandardAttributes(this.input);
-        return this;
-    }
-
-    checkBoxGenerator() {
-        this.div.addClass('form-check');
-        this.input = $('<input>', {
-            id: `${this.prefix}-input-${this.params.name}`,
-            type: 'checkbox',
-            value: '1'
-        }).addClass(`powerbeamform-input  ${this.prefix}-input form-check-input`).appendTo(this.div);
-
-        this.assignStandardAttributes(this.input);
-        if (this.params.attributes) {
-            this.input.attr(this.params.attributes);
-        }
-
-        this.label = $('<label>', { for: `${this.prefix}-input-${this.params.name}`, html: this.params.label })
-            .addClass(`powerbeamform-label  ${this.prefix}-label form-check-label`).appendTo(this.div);
-        if (this.params.required) {
-            this.label.addClass('powerbeamform-label-required');
-        }
-        return this;
-    }
-
-    radioGenerator() {
-        this.fieldset = $('<fieldset>', { html: `<legend>${this.params.label}</legend>` }).appendTo(this.div);
-        this.options = [];
-        this.input = [];
-        for (const optionParams of this.params.options) {
-
-            const option = { value: optionParams.value };
-
-            option.div = $('<div>', { id: `${this.prefix}-div-${this.params.name}-${optionParams.value}` }).addClass(`form-check`).appendTo(this.fieldset);
-            option.input = $('<input>', {
-                id: `${this.prefix}-input-${this.params.name}-${optionParams.value}`,
-                name: this.params.name,
-                value: optionParams.value,
-                type: 'radio',
-                title: optionParams.title,
-                required: this.params.required
-            }).addClass(`powerbeamform-input  ${this.prefix}-input form-check-input`).appendTo(option.div);
-
-            option.label = $('<label>', { for: `${this.prefix}-input-${this.params.name}-${optionParams.value}`, html: optionParams.label })
-                .addClass(`powerbeamform-label  ${this.prefix}-label form-check-label`).appendTo(option.div);
-
-            this.options.push(option);
-            this.input.push(option.input);
-        }
-        return this;
-    }
-
-    selectGenerator() {
-        this.input = $('<select>', { required: this.params.required }).addClass('form-select').appendTo(this.div);
-        this.options = [];
-        for (const optionParams of this.params.options) {
-            const option = $('<option>', {
-                id: `${this.prefix}-option-${this.params.name}-${optionParams.value}`,
-                name: this.params.name,
-                value: optionParams.value,
-                html: optionParams.label,
-            }).addClass(`powerbeamform-input  ${this.prefix}-input form-check-input`).appendTo(this.input);
-            this.options.push(option);
-        }
-        if (this.params.attributes) {
-            this.input.attr(this.params.attributes);
-        }
-        return this;
-    }
-
-
-    textAreaGenerator() {
-        this.label = $('<label>', { for: `${this.prefix}-input-${this.params.name}`, html: this.params.label })
-            .addClass(`powerbeamform-label  ${this.prefix}-label form-label`).appendTo(this.div);
-        if (this.params.required) {
-            this.label.addClass('powerbeamform-label-required');
-        }
-        if (this.params.validate) {
-            this.label.addClass('powerbeamform-label-validate');
-        }
-        this.input = $('<textarea>', {
-            id: `${this.prefix}-input-${this.params.name}`,
-        }).addClass(`powerbeamform-input ${this.prefix}-input form-control`).appendTo(this.div);
-        this.assignStandardAttributes(this.input);
-        if (this.params.attributes) {
-            this.input.attr(this.params.attributes);
-        }
-        return this;
-    }
-
-    customGenerator() {
-        this.form.config.custom[this.params.type].call(this);
-        return this;
-    }
-
-}
